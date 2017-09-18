488a11df9d5226be535ac51aa58956708129ab3a
Damian Michniak <damian.michniak29@gmail.com>
2017-09-18 17:27:53 -0500

    updated

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require( 'express' );
 const app = express();
 const morgan = require('morgan');
 
-app.use(morgan('dev'));
+app.use(morgannodemon);
 
 app.get('/', function (req, res) {
   res.send('hello');
